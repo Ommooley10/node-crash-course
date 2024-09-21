6c79e6fac1cf49480bd80d36a37a8b2e67552da5
Ommooley10 <150061633+Ommooley10@users.noreply.github.com>
2024-09-21 14:56:02 +0530

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8'});
+const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8'}); //either write this encoding wala part or write toString()
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
 readStream.on('data', chunk => {
   // console.log('---- NEW CHUNK ----');
-  // console.log(chunk);
+  // console.log(chunk.toString());   //either write toString or write {encoding : 'utf8} its the same anyways.
   writeStream.write('\nNEW CHUNK:\n');
-  writeStream.write(chunk);
+  writeStream.write(chunk); 
 });
 
 // piping
